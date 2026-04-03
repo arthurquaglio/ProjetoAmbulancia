ccaf457f18570a52ab5df8e04d0710a1d51562d4
Arthur Quaglio <104541718+arthurquaglio@users.noreply.github.com>
2026-04-02 21:18:21 -0300

    mais um teste A

--- app/src/main/java/com/example/projetoambulancia/MainActivity.java
+++ app/src/main/java/com/example/projetoambulancia/MainActivity.java
@@ -20,6 +20,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        System.out.println("testes");
+        System.out.println("testesA");
     }
 }
